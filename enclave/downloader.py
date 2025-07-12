7cb9f4263d14054ab7d9a8cade68c1cea9d9bb2a
Richard Fan <richardfan1126@gmail.com>
2025-07-12 19:19:31 +0800

    Shard model, push eif info to ghcr

--- enclave/downloader.py
+++ enclave/downloader.py
@@ -5,5 +5,5 @@ model_name = os.environ.get("MODEL_NAME")
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
-model.save_pretrained("./local_model")
-tokenizer.save_pretrained("./local_model")
+model.save_pretrained("./local_model", max_shard_size="1GB")
+tokenizer.save_pretrained("./local_model", max_shard_size="1GB")
